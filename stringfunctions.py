9ec1c121c38bc48b43c0a54311bfe6f9a43602e3
Navya-135 <143674020+Navya-135@users.noreply.github.com>
2023-09-26 16:21:27 +0530

    Add files via upload

---
+++ stringfunctions.py
@@ -0,0 +1,42 @@
+#String Functions
+#Capitalize
+
+#syntax: variable.capitalize()
+
+city="budawada"
+print(city.capitalize())
+
+#title
+
+#syntax:variable.title()
+
+name="navya sri"
+print(name.title())
+
+#upper
+
+#syntax:variable.upper()
+
+name="navya"
+print(name.upper())
+
+#lower
+
+#syntax:variable.lower()
+
+name="NAVYA"
+print(name.lower())
+
+#casefold
+
+#syntax:variable.casefold()
+
+name="NAVYA SRI"
+print(name.casefold())
+
+#swapcase
+
+#syntax:variable.swapcase()
+
+name="Navya Sri"
+print(name.swapcase())
